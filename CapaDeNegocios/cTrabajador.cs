92398bb79a9498ffab592a1876861394e231ec96
Edward <Edward@HP-EDIRIUS>
2016-06-14 11:20:41 -0500

    Actualizacion Trabajador

--- CapaDeNegocios/cTrabajador.cs
+++ CapaDeNegocios/cTrabajador.cs
@@ -284,6 +284,58 @@ namespace CapaDeNegocios
             }
         }
 
+        public Boolean AgregarTrabajador(cTrabajador trabajador)
+        {
+            string sexo = "";
+
+            switch (trabajador.Sexo)
+            {
+                case EnumSexo.Masculino:
+                    sexo = "M";
+                    break;
+                case EnumSexo.Femenino:
+                    sexo = "F";
+                    break;
+                default:
+
+                    break;
+            }
+            Conexion.GDatos.Ejecutar("spCrearTrabajador", trabajador.Nombres, trabajador.ApellidoPaterno, trabajador.ApellidoMaterno, sexo, trabajador.EstadoCivil.ToString(), trabajador.Direccion, trabajador.Dni, trabajador.CelularPersonal, trabajador.CelularTrabajo, trabajador.FechaNacimiento, trabajador.Foto, trabajador.CorreoElectronico, trabajador.MiTipoVia.Codigo, trabajador.NombreVia, trabajador.NumeroVia, trabajador.DepartamentoInterior, trabajador.MiTipoZOna.Codigo, trabajador.NombreZona, trabajador.Referencia, trabajador.MiDistrito.Codigo, trabajador.MiNacionalidad.Codigo);
+
+            return true;
+        }
+
+        public Boolean ModificarTrabajador(cTrabajador trabajador)
+        {
+            string sexo = "";
+
+            switch (trabajador.Sexo)
+            {
+                case EnumSexo.Masculino:
+                    sexo = "M";
+                    break;
+                case EnumSexo.Femenino:
+                    sexo = "F";
+                    break;
+                default:
+
+                    break;
+            }
+
+            Conexion.GDatos.Ejecutar("spModificarTrabajador", trabajador.IdTrabajador, trabajador.Nombres, trabajador.ApellidoPaterno, trabajador.ApellidoMaterno, sexo, trabajador.EstadoCivil.ToString(), trabajador.Direccion, trabajador.Dni, trabajador.CelularPersonal, trabajador.CelularTrabajo, trabajador.FechaNacimiento, trabajador.Foto, trabajador.CorreoElectronico, trabajador.MiTipoVia.Codigo, trabajador.NombreVia, trabajador.NumeroVia, trabajador.DepartamentoInterior, trabajador.MiTipoZOna.Codigo, trabajador.NombreZona, trabajador.Referencia, trabajador.MiDistrito.Codigo, trabajador.MiNacionalidad.Codigo);
+            return true;
+        }
+
+        public Boolean EliminarTrabajador(cTrabajador trabajador)
+        {
+            Conexion.GDatos.Ejecutar("spEliminarTrabajador", trabajador.IdTrabajador);
+            return true;
+        }
+
+        public DataTable ObtenerListaTrabajadores(Boolean Activo)
+        {
+            return Conexion.GDatos.TraerDataTable("spListarTrabajadores", Activo);
+        }
         public cAFP cAFP
         {
             get
