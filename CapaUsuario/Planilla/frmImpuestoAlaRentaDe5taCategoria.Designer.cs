c5b0265c325ee61582a15da9e83288d81f8d05ac
edward2 <Usuario@USUARIO-PC>
2016-08-15 11:48:25 -0500

    gg

---
+++ CapaUsuario/Planilla/frmImpuestoAlaRentaDe5taCategoria.Designer.cs
@@ -0,0 +1,404 @@
+﻿namespace CapaUsuario.Planilla
+{
+    partial class frmImpuestoAlaRentaDe5taCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtIRM = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtIRA = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cbMes = new System.Windows.Forms.ComboBox();
+            this.txtReMesAnteriores = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtRN = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGrati = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIngresos = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtT30 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtT20 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtT17 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtT14 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtT8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbUIT = new System.Windows.Forms.ComboBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(444, 10);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(28, 13);
+            this.label14.TabIndex = 33;
+            this.label14.Text = "UIT:";
+            // 
+            // txtIRM
+            // 
+            this.txtIRM.Location = new System.Drawing.Point(399, 491);
+            this.txtIRM.Name = "txtIRM";
+            this.txtIRM.Size = new System.Drawing.Size(65, 20);
+            this.txtIRM.TabIndex = 39;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(245, 491);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(148, 13);
+            this.label13.TabIndex = 38;
+            this.label13.Text = "Impuesto a la Renta Mensual.";
+            // 
+            // txtIRA
+            // 
+            this.txtIRA.Location = new System.Drawing.Point(399, 465);
+            this.txtIRA.Name = "txtIRA";
+            this.txtIRA.Size = new System.Drawing.Size(65, 20);
+            this.txtIRA.TabIndex = 36;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(258, 468);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(135, 13);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Impuesto a la Renta Anual:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbMes);
+            this.groupBox2.Controls.Add(this.txtReMesAnteriores);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.txtRN);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtRb);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtGrati);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtIngresos);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(45, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(463, 195);
+            this.groupBox2.TabIndex = 37;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "...";
+            // 
+            // cbMes
+            // 
+            this.cbMes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMes.FormattingEnabled = true;
+            this.cbMes.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Setiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"});
+            this.cbMes.Location = new System.Drawing.Point(220, 41);
+            this.cbMes.Name = "cbMes";
+            this.cbMes.Size = new System.Drawing.Size(121, 21);
+            this.cbMes.TabIndex = 24;
+            this.cbMes.SelectedIndexChanged += new System.EventHandler(this.cbMes_SelectedIndexChanged);
+            // 
+            // txtReMesAnteriores
+            // 
+            this.txtReMesAnteriores.Location = new System.Drawing.Point(224, 68);
+            this.txtReMesAnteriores.Name = "txtReMesAnteriores";
+            this.txtReMesAnteriores.Size = new System.Drawing.Size(65, 20);
+            this.txtReMesAnteriores.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(36, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(178, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Remuneración de Meses Anteriores:";
+            // 
+            // txtRN
+            // 
+            this.txtRN.Location = new System.Drawing.Point(223, 159);
+            this.txtRN.Name = "txtRN";
+            this.txtRN.Size = new System.Drawing.Size(65, 20);
+            this.txtRN.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Renta Neta:";
+            // 
+            // txtRb
+            // 
+            this.txtRb.Location = new System.Drawing.Point(223, 124);
+            this.txtRb.Name = "txtRb";
+            this.txtRb.Size = new System.Drawing.Size(65, 20);
+            this.txtRb.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Renta Bruta:";
+            // 
+            // txtGrati
+            // 
+            this.txtGrati.Location = new System.Drawing.Point(224, 96);
+            this.txtGrati.Name = "txtGrati";
+            this.txtGrati.Size = new System.Drawing.Size(65, 20);
+            this.txtGrati.TabIndex = 6;
+            this.txtGrati.Text = "600";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Gratificaciones:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mes:";
+            // 
+            // txtIngresos
+            // 
+            this.txtIngresos.Location = new System.Drawing.Point(220, 15);
+            this.txtIngresos.Name = "txtIngresos";
+            this.txtIngresos.Size = new System.Drawing.Size(100, 20);
+            this.txtIngresos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresos:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtT30);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.txtT20);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtT17);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtT14);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtT8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(45, 237);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 213);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tasas Aplicables ";
+            // 
+            // txtT30
+            // 
+            this.txtT30.Location = new System.Drawing.Point(219, 175);
+            this.txtT30.Name = "txtT30";
+            this.txtT30.Size = new System.Drawing.Size(65, 20);
+            this.txtT30.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(90, 175);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(107, 13);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Más de 45 UIT (30%)";
+            // 
+            // txtT20
+            // 
+            this.txtT20.Location = new System.Drawing.Point(219, 141);
+            this.txtT20.Name = "txtT20";
+            this.txtT20.Size = new System.Drawing.Size(65, 20);
+            this.txtT20.TabIndex = 18;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(169, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Más de 35 UIT hasta 45 UIT(20%)";
+            // 
+            // txtT17
+            // 
+            this.txtT17.Location = new System.Drawing.Point(219, 102);
+            this.txtT17.Name = "txtT17";
+            this.txtT17.Size = new System.Drawing.Size(65, 20);
+            this.txtT17.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(169, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Más de 20 UIT hasta 35 UIT(17%)";
+            // 
+            // txtT14
+            // 
+            this.txtT14.Location = new System.Drawing.Point(219, 62);
+            this.txtT14.Name = "txtT14";
+            this.txtT14.Size = new System.Drawing.Size(65, 20);
+            this.txtT14.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Más de 5 UIT hasta 20 UIT(14%)";
+            // 
+            // txtT8
+            // 
+            this.txtT8.Location = new System.Drawing.Point(219, 26);
+            this.txtT8.Name = "txtT8";
+            this.txtT8.Size = new System.Drawing.Size(65, 20);
+            this.txtT8.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(112, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Hasta 5 UIT(8%)";
+            // 
+            // cbUIT
+            // 
+            this.cbUIT.FormattingEnabled = true;
+            this.cbUIT.Location = new System.Drawing.Point(478, 7);
+            this.cbUIT.Name = "cbUIT";
+            this.cbUIT.Size = new System.Drawing.Size(76, 21);
+            this.cbUIT.TabIndex = 41;
+            // 
+            // frmImpuestoAlaRentaDe5taCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 523);
+            this.Controls.Add(this.cbUIT);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.txtIRM);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtIRA);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmImpuestoAlaRentaDe5taCategoria";
+            this.Text = "frmImpuestoAlaRentaDe5taCategoria";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtIRM;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtIRA;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbMes;
+        private System.Windows.Forms.TextBox txtReMesAnteriores;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtRN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGrati;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIngresos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtT30;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtT20;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtT17;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtT14;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtT8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbUIT;
+    }
+}
