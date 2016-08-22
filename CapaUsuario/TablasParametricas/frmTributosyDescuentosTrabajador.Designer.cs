92a5baca4c253bbc2ae354168dc0cf72b2fb4bb7
edward2 <Usuario@USUARIO-PC>
2016-08-22 16:54:29 -0500

    Bryans

--- CapaUsuario/TablasParametricas/frmTributosyDescuentosTrabajador.Designer.cs
+++ CapaUsuario/TablasParametricas/frmTributosyDescuentosTrabajador.Designer.cs
@@ -31,6 +31,7 @@
             this.dgvExportar = new System.Windows.Forms.DataGridView();
             this.txtIdPlanilla = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvExportar)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,11 +59,32 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // cboTipo
+            // 
+            this.cboTipo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "INGRESOS (0100)",
+            "INGRESOS : ASIGNACIONES (0200)",
+            "INGRESOS : BONIFICACIONES (0300)",
+            "INGRESOS : GRATIFICACIONES/AGUINALDOS (0400)",
+            "INGRESOS : INDEMNIZACIONES (0500)",
+            "CONCEPTOS VARIOS (0900)",
+            "OTROS CONCEPTOS (1000)",
+            "RÉGIMEN LABORAL PÚBLICO (2000)"});
+            this.cboTipo.Location = new System.Drawing.Point(31, 12);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(455, 21);
+            this.cboTipo.TabIndex = 3;
+            // 
             // frmTributosyDescuentosTrabajador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(615, 465);
+            this.Controls.Add(this.cboTipo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtIdPlanilla);
             this.Controls.Add(this.dgvExportar);
@@ -79,5 +101,6 @@
         private System.Windows.Forms.DataGridView dgvExportar;
         private System.Windows.Forms.TextBox txtIdPlanilla;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cboTipo;
     }
 }
