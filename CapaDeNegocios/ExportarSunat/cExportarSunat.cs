1f94510f3a330db271c9ee6a6ede26b973d558a7
edward2 <Usuario@USUARIO-PC>
2016-08-25 16:50:51 -0500

    Bryans

--- CapaDeNegocios/ExportarSunat/cExportarSunat.cs
+++ CapaDeNegocios/ExportarSunat/cExportarSunat.cs
@@ -12,11 +12,7 @@ namespace CapaDeNegocios.ExportarSunat
 {
     public class cExportarSunat
     {
-        public DataTable ListarTrabajadoresPorPlanilla(int id)
-        {
-            return Conexion.GDatos.TraerDataTable("spListarTrabajadoresPlanilla", id);
-        }
-
+       
         public string ExportarTexto(string tipoDoc, string dni, string codigo, string montodevengado, string monto)
         {
             
@@ -31,7 +27,23 @@ namespace CapaDeNegocios.ExportarSunat
             string ConcatenarTitulo = codigoform + año + mes + ruc + ".rem";
             return ConcatenarTitulo;
         }
-
+        public DataTable ListarTrabajadoresPorPlanilla(string numero)
+        {
+            return Conexion.GDatos.TraerDataTable("spExportarTributosTrabajadores", numero);
+        }
+        //spListarMaestroIngresosporTipo
+        public DataTable ListarMaestroIngresosxTipo(string Tipo)
+        {
+            return Conexion.GDatos.TraerDataTable("spListarMaestroIngresosporTipo", Tipo);
+        }
+        public DataTable ListarPlanillas()
+        {
+            return Conexion.GDatos.TraerDataTable("spListarPlanillas");
+        }
+        public DataTable BuscarPlanillas(string mes, string año)
+        {
+            return Conexion.GDatos.TraerDataTable("spBuscarPlanillas", mes, año);
+        }
 
     }
 }
