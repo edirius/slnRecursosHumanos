0924ee377bc5d9de1e768177b210730c0fe0c317
Edward <Edward@HP-EDIRIUS>
2016-08-26 09:41:06 -0500

    Modificacion del trabajado rpara essalud vida

---
+++ CapaDeNegocios/Usuario/cUsuario.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDeDatos;
+using System.Data;
+
+namespace CapaDeNegocios.Usuario
+{
+    public class cUsuario
+    {
+        int codigo;
+        string nombre;
+        string password;
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+
+        public DataTable ListaDepartamentos()
+        {
+            return Conexion.GDatos.TraerDataTable("spListarUsuarios");
+        }
+    }
+}
