a386539779313fcd217c0af15dacb4ebae13fd4e
edward2 <Usuario@USUARIO-PC>
2016-10-18 08:48:53 -0500

    TRAMITE

---
+++ CapaDeUsuarioTramite/FrmMenu.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace CapaDeUsuarioTramite
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenu));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblnormal = new System.Windows.Forms.Label();
+            this.lblmaximizar = new System.Windows.Forms.Label();
+            this.lbleliminar = new System.Windows.Forms.Label();
+            this.lblminimizar = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(0, 54);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(974, 388);
+            this.tabControl1.TabIndex = 93;
+            this.tabControl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabControl1_DrawItem);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.ForeColor = System.Drawing.Color.Black;
+            this.tabPage1.ImageIndex = 0;
+            this.tabPage1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.tabPage1.Location = new System.Drawing.Point(97, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabPage1.Size = new System.Drawing.Size(873, 380);
+            this.tabPage1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.ImageIndex = 1;
+            this.tabPage2.Location = new System.Drawing.Point(97, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabPage2.Size = new System.Drawing.Size(873, 380);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "LogoFaceBook.png");
+            this.imageList1.Images.SetKeyName(1, "icono-domicilio.png");
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 442);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(974, 22);
+            this.statusStrip1.TabIndex = 92;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.30517F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 93.69483F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 27);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(974, 27);
+            this.tableLayoutPanel1.TabIndex = 91;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.lblnormal);
+            this.panel1.Controls.Add(this.lblmaximizar);
+            this.panel1.Controls.Add(this.lbleliminar);
+            this.panel1.Controls.Add(this.lblminimizar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(974, 27);
+            this.panel1.TabIndex = 90;
+            // 
+            // lblnormal
+            // 
+            this.lblnormal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblnormal.AutoSize = true;
+            this.lblnormal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblnormal.ForeColor = System.Drawing.Color.Black;
+            this.lblnormal.Location = new System.Drawing.Point(933, 7);
+            this.lblnormal.Name = "lblnormal";
+            this.lblnormal.Size = new System.Drawing.Size(19, 13);
+            this.lblnormal.TabIndex = 24;
+            this.lblnormal.Text = "❐";
+            this.lblnormal.Visible = false;
+            this.lblnormal.Click += new System.EventHandler(this.lblnormal_Click);
+            // 
+            // lblmaximizar
+            // 
+            this.lblmaximizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblmaximizar.AutoSize = true;
+            this.lblmaximizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblmaximizar.ForeColor = System.Drawing.Color.Black;
+            this.lblmaximizar.Location = new System.Drawing.Point(936, 7);
+            this.lblmaximizar.Name = "lblmaximizar";
+            this.lblmaximizar.Size = new System.Drawing.Size(14, 13);
+            this.lblmaximizar.TabIndex = 23;
+            this.lblmaximizar.Text = "□";
+            this.lblmaximizar.Click += new System.EventHandler(this.lblmaximizar_Click);
+            // 
+            // lbleliminar
+            // 
+            this.lbleliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbleliminar.AutoSize = true;
+            this.lbleliminar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbleliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbleliminar.ForeColor = System.Drawing.Color.Black;
+            this.lbleliminar.Location = new System.Drawing.Point(956, 7);
+            this.lbleliminar.Name = "lbleliminar";
+            this.lbleliminar.Size = new System.Drawing.Size(15, 13);
+            this.lbleliminar.TabIndex = 22;
+            this.lbleliminar.Text = "X";
+            this.lbleliminar.Click += new System.EventHandler(this.lbleliminar_Click);
+            // 
+            // lblminimizar
+            // 
+            this.lblminimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblminimizar.AutoSize = true;
+            this.lblminimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblminimizar.ForeColor = System.Drawing.Color.Black;
+            this.lblminimizar.Location = new System.Drawing.Point(916, 3);
+            this.lblminimizar.Name = "lblminimizar";
+            this.lblminimizar.Size = new System.Drawing.Size(13, 13);
+            this.lblminimizar.TabIndex = 21;
+            this.lblminimizar.Text = "_";
+            this.lblminimizar.Click += new System.EventHandler(this.lblminimizar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 111);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 464);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMenu";
+            this.Text = "FrmMenu";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblnormal;
+        private System.Windows.Forms.Label lblmaximizar;
+        private System.Windows.Forms.Label lbleliminar;
+        private System.Windows.Forms.Label lblminimizar;
+        public System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ CapaDeUsuarioTramite/Tramite/frmMenuDinamico.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDeUsuarioTramite.Tramite
+{
+    public partial class frmMenuDinamico : Form
+    {
+        public frmMenuDinamico()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            TabPage CurrentTab = tabControl1.TabPages[e.Index];
+            Rectangle ItemRect = tabControl1.GetTabRect(e.Index);
+            SolidBrush FillBrush = new SolidBrush(Color.Red);
+            SolidBrush TextBrush = new SolidBrush(Color.White);
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+
+            //If we are currently painting the Selected TabItem we'll
+            //change the brush colors and inflate the rectangle.
+            if (System.Convert.ToBoolean(e.State & DrawItemState.Selected))
+            {
+                FillBrush.Color = Color.White;
+                TextBrush.Color = Color.Red;
+                ItemRect.Inflate(2, 2);
+            }
+
+            //Set up rotation for left and right aligned tabs
+            if (tabControl1.Alignment == TabAlignment.Left || tabControl1.Alignment == TabAlignment.Right)
+            {
+                float RotateAngle = 90;
+                if (tabControl1.Alignment == TabAlignment.Left)
+                    RotateAngle = 270;
+                PointF cp = new PointF(ItemRect.Left + (ItemRect.Width / 2), ItemRect.Top + (ItemRect.Height / 2));
+                e.Graphics.TranslateTransform(cp.X, cp.Y);
+                e.Graphics.RotateTransform(RotateAngle);
+                ItemRect = new Rectangle(-(ItemRect.Height / 2), -(ItemRect.Width / 2), ItemRect.Height, ItemRect.Width);
+            }
+
+            //Next we'll paint the TabItem with our Fill Brush
+            e.Graphics.FillRectangle(FillBrush, ItemRect);
+
+            //Now draw the text.
+            e.Graphics.DrawString(CurrentTab.Text, e.Font, TextBrush, (RectangleF)ItemRect, sf);
+
+            //Reset any Graphics rotation
+            e.Graphics.ResetTransform();
+
+            //Finally, we should Dispose of our brushes.
+            FillBrush.Dispose();
+            TextBrush.Dispose();
+
+        }
+    }
+}
